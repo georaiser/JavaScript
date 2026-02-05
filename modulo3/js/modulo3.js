1b760b536fef58ddb0fb114e260c346666715650
Jorge Rodriguez <jrodrigueze.info@gmail.com>
2026-02-05 00:15:11 -0300

    update modulo3

--- modulo3/js/modulo3.js
+++ modulo3/js/modulo3.js
@@ -16,52 +16,69 @@ if (userName !== null && userName !== "") {
 
 // Create variables that store numbers, text, and boolean values
 const pi = "3.14159";             // pi variable (constant)
-// let isStudent = true;             // Boolean variable
 
-let userAge = prompt("enter your age:", "18");    
+let isStudent = confirm("Are you a student?");
 
-if (userAge !== null && userAge !== "" && userAge >= 18) {
-    console.log("Hello, " + userName + "! You are " + userAge + " years old.");
-    alert("Hello, " + userName + "! You are " + userAge + " years old.");
+if (isStudent) {
 
-    let isStudent = confirm("Are you a student?");
+    console.log("Hello, " + userName + "! You are a student.");
+    alert("Hello, " + userName + "! You are a student.");
 
-    if (isStudent) {
-        console.log("As a student, you could calculate cylinder area and volume: ");
-        alert("As a student, you could calculate the cylinder area and volume: ");
-
-        const imgSrc = "./img/cilindro.png";
-        const img = document.createElement('img');
-        img.src = imgSrc;
-        img.alt = "Formulas del área y volumen de un cilindro";
-        document.body.appendChild(img);
-
-        // Use setTimeout to allow the image to render before blocking with prompt
-        setTimeout(function() {
-            let radius = parseFloat(prompt("Enter the radius of the cylinder:"));
-            let height = parseFloat(prompt("Enter the height of the cylinder:"));
-            
-            let area = 2 * pi * radius * (radius + height);
-            let volume = pi * radius ** 2 * height;
-            
-            console.log("The area of the cylinder is: " + area.toFixed(2));
-            console.log("The volume of the cylinder is: " + volume.toFixed(2));
-            alert("The area of the cylinder is: " + area.toFixed(2));
-            alert("The volume of the cylinder is: " + volume.toFixed(2));
-
-            // Remove the image after the calculation is done
-            img.remove();
-            
-            alert("Good bye.");
-        }, 100);
-
-
-    }
+    let geometricFigure = prompt("enter geometric figure (cylinder, cone, sphere):", "cylinder");    
     
-} else if (userAge !== null && userAge !== "" && userAge < 18) {
-    console.log("Hello, " + userName + "! You are a minor at " + userAge + " years old, good bye.");
-    alert("Hello, " + userName + "! You are a minor at " + userAge + " years old, good bye.");
+    if (geometricFigure !== null && geometricFigure !== "") {
+
+        switch (geometricFigure) {
+            case "cylinder":
+                console.log("As a student, you could calculate cylinder area and volume: ");
+                alert("As a student, you could calculate the cylinder area and volume: ");
+
+                const imgSrc = "./img/cilindro.png";
+                const img = document.createElement('img');
+                img.src = imgSrc;
+                img.alt = "Formulas de área y volumen de un cilindro";
+                // add image to body
+                document.body.appendChild(img);
+
+                // Use setTimeout to allow the image to render before blocking with prompt
+                setTimeout(function() {
+                    let radius = parseFloat(prompt("Enter the radius of the cylinder:"));
+                    let height = parseFloat(prompt("Enter the height of the cylinder:"));
+                    
+                    let area = 2 * pi * radius * (radius + height);
+                    let volume = pi * radius ** 2 * height;
+                    
+                    console.log("The area of the cylinder is: " + area.toFixed(2));
+                    console.log("The volume of the cylinder is: " + volume.toFixed(2));
+                    alert("The area of the cylinder is: " + area.toFixed(2));
+                    alert("The volume of the cylinder is: " + volume.toFixed(2));
+
+                    // Remove the image after the calculation is done
+                    img.remove();
+                    
+                    // alert("Good bye.");
+                }, 100);
+
+                break;
+
+            case "cone":
+                console.log("As a student, you could calculate cone area and volume: ");
+                alert("As a student, you could calculate the cone area and volume: ");
+                break;
+
+            case "sphere":
+                console.log("As a student, you could calculate sphere area and volume: ");
+                alert("As a student, you could calculate the sphere area and volume: ");
+                break;
+            default:
+                console.log("wrong geometric figure value");
+                alert("You didn't enter a valid geometric figure.");
+                break;
+        }
+    }
+
 } else {
-    console.log("wrong age value");
-    alert("You didn't enter a valid age.");
+    console.log("you are not an student, good bye");
+    alert("You are not an student, good bye.");
 }
+
